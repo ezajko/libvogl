6a90d13fc041b15402384fe0c03ad5882188a56d
Ernedin Zajko <ezajko@root.ba>
2019-11-18 16:05:34 +0100

    cleaning drivers...

--- drivers/builddev.bat
+++
@@ -1,4 +0,0 @@
-rem
-rem This is another variant on building with djgpp....
-rem
-make -f makefile.dj2 DEVICES="-DPOSTSCRIPT -DHPGL -DGRX" MCFLAGS=-g2 DOBJS="../drivers/ps.o ../drivers/hpdxy.o ../drivers/grx.o" RANLIB=ranlib
